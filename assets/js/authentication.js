ecefc3c1a6fc0f825292a6a73455875e168a029a
Kyle Brandon Sioson <siosonkylebrandon@gmail.com>
2017-10-02 23:33:54 +0800

    stem2

---
+++ assets/js/authentication.js
@@ -0,0 +1,15 @@
+<script language="JavaScript" type="text/javascript">
+<!--
+function checkform ( form )
+{
+ 
+  if (form.email.value == "") {
+    alert( "Please enter your email address." );
+    form.email.focus();
+    return false ;
+  }
+
+  return true ;
+}
+
+</script>
